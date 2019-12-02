bc9b07bf29b6478c6105e99c8bfa2a53f970a88e
Swaleh Mhina <cyph09@gmail.com>
2019-12-02 21:20:59 +0300

    Editor and Previewer

---
+++ src/components/Editor.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Editor extends Component {
+  render() {
+    return (
+      <div>
+        <form>
+          <input type="text" name="editor" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Editor;
---
+++ src/components/Previewer.js
@@ -0,0 +1,8 @@
+import React from "react";
+const Previewer = props => (
+  <div>
+    <h4>Previewer</h4>
+  </div>
+);
+
+export default Previewer;
