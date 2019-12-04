f61f39b5a239c896ca52b166011b846c6558ea49
Swaleh Mhina <cyph09@gmail.com>
2019-12-04 15:25:28 +0300

    Updates

---
+++ precache-manifest.9e8c9d8d71f07221e753793e0392ee34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9573e08da2aa6df5a49cc77a0a2aa5d1",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "6fa793b589522ffee2e6",
+    "url": "/markdown-previewer/static/css/main.aa855dda.chunk.css"
+  },
+  {
+    "revision": "04c1a272d86967b6e3e3",
+    "url": "/markdown-previewer/static/js/2.e68c8071.chunk.js"
+  },
+  {
+    "revision": "6fa793b589522ffee2e6",
+    "url": "/markdown-previewer/static/js/main.1ee96693.chunk.js"
+  },
+  {
+    "revision": "9c8829833ff1846aa2df",
+    "url": "/markdown-previewer/static/js/runtime-main.6602ab49.js"
+  }
+]);
